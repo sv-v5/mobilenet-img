b14a5bbe1dbc692aada72f9e51b2178953498551
sv-v5 <svyatver@gmail.com>
2021-06-23 19:18:51 -0500

    Updates

---
+++ precache-manifest.7038d1ec24ac0e1e6980a6c0677d0d4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f27eb1026a7127f1ae0cf64c876892",
+    "url": "/mobilenet-img/index.html"
+  },
+  {
+    "revision": "5e4e9e367559bdcc9110",
+    "url": "/mobilenet-img/static/css/main.f67bec75.chunk.css"
+  },
+  {
+    "revision": "76f6de029ebc4dbc96d1",
+    "url": "/mobilenet-img/static/js/2.024851f0.chunk.js"
+  },
+  {
+    "revision": "41e4562c5f0523080dee48b00c57385d",
+    "url": "/mobilenet-img/static/js/2.024851f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4e9e367559bdcc9110",
+    "url": "/mobilenet-img/static/js/main.29ad72d3.chunk.js"
+  },
+  {
+    "revision": "6f131d3816fb5fa6423b",
+    "url": "/mobilenet-img/static/js/runtime-main.01027b7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobilenet-img/static/media/logo.5d5d9eef.svg"
+  }
+]);
